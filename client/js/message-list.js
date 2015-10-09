16e62cc4ea40c6b0f0bc5515612777feb9078edd
Joe Innes <joe@joeinn.es>
2015-10-09 18:39:08 +0000

    Removed console logging

--- client/js/message-list.js
+++ client/js/message-list.js
@@ -23,14 +23,12 @@ Template.messagelist.onCreated(function () {
     });
     var timer;
     $(window).blur(function () {
-      console.log('Observing');
       var initialising = true;
       Messages.observer = Messages.find({
         channel: Session.get('currentChannel')
       }).observe({
         added: function (doc) {
           if (!initialising) {
-            console.log('A document was added!');
             timer = window.setInterval(function() {
               window.document.title = window.document.title == "Mokus" ? "New message..." : "Mokus";
             }, 800);
@@ -41,7 +39,6 @@ Template.messagelist.onCreated(function () {
     });
 
     $(window).focus(function () {
-      console.log('No longer observing');
       if (Messages.observer) {
         Messages.observer.stop(); // Call the stop
       }
