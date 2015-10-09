a42ada0cadca12db1ba70e392ae8a22b317ef752
Joe Innes <joe@joeinn.es>
2015-10-09 18:37:18 +0000

    Enabled flashing window title. Closes #11

--- client/js/message-list.js
+++ client/js/message-list.js
@@ -2,14 +2,51 @@
 /* globals Messages, Session, Template */
 
 Template.messagelist.helpers({
-  messages: function() {
-    return Messages.find({channel: Session.get('currentChannel')}, {sort: {createdAt: -1}});
+  messages: function () {
+    return Messages.find({
+      channel: Session.get('currentChannel')
+    }, {
+      sort: {
+        createdAt: -1
+      }
+    });
   },
 });
 
-Template.messagelist.onCreated(function() {
+Template.messagelist.onCreated(function () {
   var self = this;
-  self.autorun(function() {
-    self.subscribe('messages', Session.get('currentChannel'), {sort: {createdAt: -1}});
+  self.autorun(function () {
+    self.subscribe('messages', Session.get('currentChannel'), {
+      sort: {
+        createdAt: -1
+      }
+    });
+    var timer;
+    $(window).blur(function () {
+      console.log('Observing');
+      var initialising = true;
+      Messages.observer = Messages.find({
+        channel: Session.get('currentChannel')
+      }).observe({
+        added: function (doc) {
+          if (!initialising) {
+            console.log('A document was added!');
+            timer = window.setInterval(function() {
+              window.document.title = window.document.title == "Mokus" ? "New message..." : "Mokus";
+            }, 800);
+          }
+        }
+      });
+      initialising = false;
+    });
+
+    $(window).focus(function () {
+      console.log('No longer observing');
+      if (Messages.observer) {
+        Messages.observer.stop(); // Call the stop
+      }
+      clearInterval(timer);
+      window.document.title = 'Mokus';
+    });
   });
 });
