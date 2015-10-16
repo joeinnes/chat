c340468a28de6152029b703923fbe0d94dab0d40
Joe Innes <joe@joeinn.es>
2015-10-16 18:02:14 +0000

    Added infinite scroll

--- client/js/config.js
+++ client/js/config.js
@@ -1,9 +1,13 @@
 /* jshint strict:false */
-/* globals Accounts, Avatar */
+/* globals ItemsIncrement, ReactiveTimer, Accounts, Avatar */
 
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL',
 });
 
+ItemsIncrement = 20;
+Session.setDefault('itemsLimit', ItemsIncrement);
+
 ReactiveTimer = new ReactiveTimer();
 ReactiveTimer.start(10);
+
