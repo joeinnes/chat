c883b1ccd4e7e19fd933f79e4d018e4e1855eaf7
Joe Innes <joe@joeinn.es>
2015-10-23 18:02:08 +0000

    Think I cracked notifications. Added HTML5 notifications API, and flashing screen

--- client/js/message-list.js
+++ client/js/message-list.js
@@ -17,77 +17,52 @@ Template.messagelist.helpers({
 Template.messagelist.onCreated(function () {
   var self = this;
   self.autorun(function () {
-    self.subscribe('messages', Session.get('currentChannel'), Session.get('itemsLimit'), {});
+    var initialising = true;
     var timer;
 
-    $(window).on("blur focus", function (e) {
-      var prevType = $(this).data("prevType");
+    self.subscribe('messages', Session.get('currentChannel'), Session.get('itemsLimit'), {});
+    self.subscribe('notifications');
 
-      if (prevType != e.type) { //  reduce double fire issues
-        switch (e.type) {
-        case "blur":
-          /*var initialising = true;
-          Messages.observer = Messages.find({
-            channel: Session.get('currentChannel')
-          }).observe({
-            added: function (doc) {
-              if (!initialising) {
-                timer = window.setInterval(function () {
-                  window.document.title = window.document.title == "Mokus" ? "New message..." : "Mokus";
-                }, 800);
-              }
+    Notifications.observer = Notifications.find({
+      fao: Meteor.userId(),
+      read: false,
+    }).observe({
+      added: function (doc) {
+        if (!initialising) {
+          if (!document.hasFocus()) {
+            notifyMe(doc);
+          } else {
+            // Otherwise, find out whether the user is looking at a channel or a user
+            if (Router.current().route.getName() === 'channel') {
+              // If the user is looking at a channel, mark all notifications from that channel as read
+              Meteor.call('readNotification', 'channel', Session.get('currentChannel'));
+            } else if (Router.current().route.getName() === 'user') {
+              // If the user is looking at a user, mark all notifications from that user as read
+              Meteor.call('readNotification', 'user', Session.get('userview'));
             }
-          });
-          initialising = false;*/
-         var initialising = true;
-         Notifications.observer = Notifications.find({
-            fao: Meteor.userId(),
-            read: false,
-          }).observe({
-            added: function (doc) {
-              timer = window.setInterval(function () {
-                window.document.title = window.document.title === "Mokus" ? "New notifications..." : "Mokus";
-              }, 500);
-            }
-          });
-          initialising = false;
-
-          unreadNotifications = Notifications.find({fao: Meteor.userId(), read: false}).count();
-          if (unreadNotifications > 0) {
-
           }
-          break;
-        case "focus":
-          var initialising = true;
-         Notifications.observer = Notifications.find({
-            fao: Meteor.userId(),
-            read: false,
-          }).observe({
-            added: function (doc) {
-              if (!initialising) {
-                Meteor.call('readNotification', doc._id);
-              }
-            }
-          });
-          initialising = false;
-
-
-          if (Router.current().route.getName() ===  'channel') {
-            Meteor.call('readNotification', 'channel', Session.get('currentChannel'));
-          } else if (Router.current().route.getName() ===  'user') {
-            Meteor.call('readNotification', 'user', Session.get('userview'));
-          }
-
-          clearInterval(timer);
-          window.document.title = 'Mokus';
-          break;
         }
       }
+    });
 
-      $(this).data("prevType", e.type);
-    })
+    initialising = false;
+
+
+    $(window).on('focus', function () {
+      FlashTitle.stop();
+      // When the window gets focus, check what the user is looking at
+      if (Router.current().route.getName() === 'channel') {
+        // If the user is looking at a channel, mark all notifications from that channel as read
+        Meteor.call('readNotification', 'channel', Session.get('currentChannel'));
+      }
+      else if (Router.current().route.getName() === 'user') {
+        // If the user is looking at a user, mark all notifications from that user as read
+        Meteor.call('readNotification', 'user', Session.get('userview'));
+      }
+    });
   });
 });
+
 
 function showMoreVisible() {
   var threshold, target = $("#showMoreResults");
@@ -111,3 +86,45 @@ function showMoreVisible() {
 
 // run the above func every time the user scrolls
 $(window).scroll(showMoreVisible);
+
+function notifyMe(doc) {
+  // Let's check if the browser supports notifications
+  var options = {
+      body: doc.message.createdBy.username + ' said ' + doc.message.text,
+  }
+
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("New notification", options);
+    FlashTitle.start();
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("New notification", options);
+        FlashTitle.start();
+      }
+    });
+  }
+}
+
+FlashTitle = {
+  start: function() {
+    FlashTitle.timer=window.setInterval(function() {
+      document.title = document.title == "Mokus" ? "New notification..." : "Mokus";
+    }, 1000);
+  },
+  stop: function() {
+    document.title = "Mokus";
+    clearInterval(FlashTitle.timer);
+  },
+  timer: '',
+}
